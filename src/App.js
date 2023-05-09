281f9c9ac9331422fbd291713030a34730a28008
dilnabz <artikovadilnoza19@gmail.com>
2023-05-09 20:27:35 +0300

    add logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import { Dropdown } from './components/Dropdown.js';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+// CSS Modules, react-datepicker-cssmodules.css// 
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import { registerLocale, setDefaultLocale } from  "react-datepicker";
+import ru from 'date-fns/locale/ru';
+import "./datePickerStyle.css"
+registerLocale('ru', ru);
+setDefaultLocale('ru', ru);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+export function App() {
+	const [startDate, setStartDate] = useState("");
+	const [startTime, setStartTime] = useState("");
+	const [endTime, setEndTime] = useState("");
+	const [selectedTower, setSelectedTower] = useState(null);
+	const [selectedFloor, setSelectedFloor] = useState(null);
+	const [selectedMeetingRoom, setSelectedMeetingRoom] = useState(null);
+
+	function handleSubmit() {
+		const data = {
+			tower: selectedTower,
+			floor: selectedFloor,
+			meetingRoom: selectedMeetingRoom,
+			date: startDate,
+			startTime: startTime,
+			endTime: endTime
+		}
+		console.log(JSON.stringify(data));
+	};
+
+	function clearAll() {
+		setStartDate("");
+		setStartTime("");
+		setEndTime("");
+		setSelectedTower(null);
+		setSelectedFloor(null);
+		setSelectedMeetingRoom(null);
+	};
+
+	const towers = [
+		{value: "towerA", label: "Башня А"},
+		{value: "towerB", label: "Башня Б"},
+	];
+  	const floors = Array(27).fill().map((_, i) => {
+		return {value: `floor${i+1}`, label: `${i+1} этаж`};
+  	}).slice(2);
+
+  	const meetingRooms = Array(10).fill().map((_, i) => {
+		return {value: `room${i+1}`, label: `${i+1} переговорная`};
+  	});
+
+  	return (      
+    <div className='app'>
+    	<div>
+    		<Dropdown 
+				placeHolder="Башня"
+				options={towers}
+				selected={selectedTower}
+				onSelectedChange={setSelectedTower}
+			/>
+    	</div>
+		<div>
+    		<Dropdown 
+				placeHolder="Этаж"
+				options={floors}
+				selected={selectedFloor}
+				onSelectedChange={setSelectedFloor}
+			/>
+    	</div>
+		<div>
+    		<Dropdown 
+				placeHolder="Переговорная"
+				options={meetingRooms}
+				selected={selectedMeetingRoom}
+				onSelectedChange={setSelectedMeetingRoom}
+			/>
+    	</div>
+    	<div>
+        	<DatePicker
+        		locale="ru"    
+        		selected={startDate}         
+        		onChange={(date) => setStartDate(date)}
+				placeholderText="Выберите дату"          
+        	/>
+    	</div>
+		<div>
+			<DatePicker
+      			selected={startTime}
+      			onChange={(date) => setStartTime(date)}
+      			showTimeSelect
+      			showTimeSelectOnly
+      			timeIntervals={15}
+      			timeCaption="Time"
+      			dateFormat="HH:mm"
+				placeholderText="Начало"
+    		/>
+		</div>
+		<div>
+			<DatePicker
+      			selected={endTime}
+      			onChange={(date) => setEndTime(date)}
+      			showTimeSelect
+      			showTimeSelectOnly
+      			timeIntervals={15}
+      			timeCaption="Time"
+      			dateFormat="HH:mm"
+				placeholderText="Конец"
+    		/>
+		</div>
+		<button onClick={handleSubmit} className="submitBtn">Отправить</button>
+		<button onClick={clearAll} className="clearBtn">Очистить</button>
+    </div> 
   );
-}
-
-export default App;
+};
---
+++ src/components/Dropdown.js
@@ -0,0 +1,74 @@
+import React, {useState, useEffect, useRef} from "react";
+import "./Dropdown.css";
+
+export function Dropdown({ 
+    placeHolder, 
+    options, 
+    selected, 
+    onSelectedChange 
+}) {
+    const [showMenu, setShowMenu] = useState(false);
+    const inputRef = useRef();
+
+    useEffect(() => {
+        const handler = (e) => {
+            if (inputRef.current && !inputRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+        window.addEventListener("click", handler);
+
+        return () => {
+            window.removeEventListener("click", handler);
+        };
+    }, [showMenu]);
+
+    const handleInputClick = () => {
+        setShowMenu(!showMenu);
+    };
+
+    const getDisplay = () => {
+        if (selected) {
+            return selected.label;
+        };
+        return placeHolder;
+    };
+
+    const onItemClick = (option) => {
+        onSelectedChange(option);
+    };
+
+    const isSelected = (option) => {
+        if (!selected) {
+            return false;
+        };
+
+        return selected.value === option.value;
+    };
+
+    return (
+        <div className="dropdown-container">
+            <div ref={inputRef} onClick={handleInputClick} className="dropdown-input">
+                <div className="dropdown-selected-value">{getDisplay()}</div>
+                {/* <div className="dropdown-tools">
+                    <div className="dropdown-tool">
+                        
+                    </div>
+                </div> */}
+            </div>
+            {showMenu && (<div className="dropdown-menu">
+                {options.map((option) => {
+                    return (
+                        <div 
+                            onClick={() => onItemClick(option)}
+                            key={option.value} 
+                            className={`dropdown-item ${isSelected(option) && "selected"}`}
+                        >
+                            {option.label}
+                        </div>
+                    )
+                })}
+            </div>)}
+        </div>
+    );
+};
